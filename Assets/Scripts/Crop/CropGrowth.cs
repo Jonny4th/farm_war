e13b64d528d49071183049744636705fdf933ec5
BluePained <inwisetjiro666@gmail.com>
2024-02-16 05:42:04 +0700

    CropGrowingScript

--- Assets/Scripts/Crop/CropGrowth.cs
+++ Assets/Scripts/Crop/CropGrowth.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class CropGrowth : MonoBehaviour
 {
+    [SerializeField] private MeshFilter meshFilter;
+    [SerializeField] private SOcrop _soCrop;
+    [SerializeField] private int cropCurrentState;
+    [SerializeField] private float cropTimer;
+    [SerializeField] private GameObject particle;
+    [SerializeField] private bool cropIsReady;
+    [SerializeField] private bool particleIsPlay;
     // Start is called before the first frame update
     void Start()
     {
-        
+        particleIsPlay = false;
+        cropIsReady = false;
+        cropCurrentState = 0;
+        meshFilter = GetComponent<MeshFilter>();
+        meshFilter.mesh = _soCrop._cropstate[cropCurrentState];
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(cropCurrentState != _soCrop._cropstate.Length - 1)
+        {
+            cropTimer += Time.deltaTime;
+        }
+        else
+        {
+            cropIsReady = true;
+        }
+
+        if(cropIsReady == true && particleIsPlay == false)
+        {
+            CropPartical();
+            particleIsPlay = true;
+        }
         
+
+        if(cropTimer >= _soCrop._timegrowth)
+        {
+            CropGrowing();
+        }
+    }
+
+    private void CropGrowing()
+    {
+        cropTimer = 0;
+        cropCurrentState += 1;
+        meshFilter.mesh = _soCrop._cropstate[cropCurrentState];
+    }
+
+    private void CropPartical()
+    {
+        Instantiate(particle, gameObject.transform.position,Quaternion.identity);
     }
 }
--- Assets/Scripts/Crop/SOcrop.cs
+++ Assets/Scripts/Crop/SOcrop.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "Crop", menuName = "ScriptableObject/CropScriptableObject")]
 public class SOcrop : ScriptableObject
-{   
+{
+    [SerializeField] private string cropName;
+    [SerializeField] private float timeGrowth;
+    [SerializeField] private Mesh[] cropState;
 
+    public float _timegrowth => timeGrowth;
+    public Mesh[] _cropstate => cropState;
 }
