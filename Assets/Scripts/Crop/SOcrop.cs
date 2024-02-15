07d15e39a50751b10c91c2369c0862dc41968b84
BluePained <inwisetjiro666@gmail.com>
2024-02-16 03:54:34 +0700

    SOcrop

---
+++ Assets/Scripts/Crop/SOcrop.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Crop", menuName = "ScriptableObject/CropScriptableObject")]
+public class SOcrop : ScriptableObject
+{   
+
+}
