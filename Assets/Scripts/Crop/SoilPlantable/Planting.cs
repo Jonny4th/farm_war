d0731e438cbab121ec3b4f102d442bac89e3d3bb
BluePained <inwisetjiro666@gmail.com>
2024-02-16 06:25:48 +0700

    PlantOnSoil

---
+++ Assets/Scripts/Crop/SoilPlantable/Planting.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planting : MonoBehaviour
+{
+    [SerializeField] private GameObject[] crops;
+    [SerializeField] private bool planted;
+    // Start is called before the first frame update
+    void Start()
+    {
+        planted = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && planted == false)
+        {
+            PlantTheCrop();
+        }
+    }
+
+    private void PlantTheCrop()
+    {
+        planted = true;
+        Instantiate(crops[0], new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 2, gameObject.transform.position.z), Quaternion.identity );
+    }
+}
