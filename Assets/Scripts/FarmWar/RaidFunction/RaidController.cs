97bd6a916e4e6d5cd8c15491271372038d81c825
Kajornpop Toboonchuay <jonny4th@gmail.com>
2024-02-22 22:02:33 +0700

    attach raid controller to button

--- Assets/Scripts/FarmWar/RaidFunction/RaidController.cs
+++ Assets/Scripts/FarmWar/RaidFunction/RaidController.cs
@@ -3,28 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class RaidTestScript : MonoBehaviour
+public class RaidController : MonoBehaviour
 {
-    public Raidable[] m_targetList;
-    public List<Raid> m_raidList = new();
+    public Raidable[] m_TargetList;
+    public List<Raid> m_RaidList = new();
 
     [SerializeField]
-    private RaidSpawner raidController;
+    private RaidSpawner m_Spawner;
 
     [SerializeField]
-    private Transform raidParent;
-
-    void Update()
+    private Transform m_RaidParent;
+    public void RandomSpawnOnGround()
     {
-        if(Input.GetKeyUp(KeyCode.Mouse0))
-        {
-            RandomSpawnOnGround();
-        }
-    }
-
-    private void RandomSpawnOnGround()
-    {
-        var raidables = Array.FindAll(m_targetList, x => x.IsRaidable && !x.IsFullyOccupied);
+        var raidables = Array.FindAll(m_TargetList, x => x.IsRaidable && !x.IsFullyOccupied);
         if(raidables.Length == 0) return;
 
         var target = raidables[Random.Range(0, raidables.Length)];
@@ -37,8 +28,8 @@ public class RaidTestScript : MonoBehaviour
         }
         else
         {
-            raid = raidController.Spawn(pos, raidParent);
-            m_raidList.Add(raid);
+            raid = m_Spawner.Spawn(pos, m_RaidParent);
+            m_RaidList.Add(raid);
         }
 
         raid.OnRaidCompleted.AddListener(RaidCompleteHandler);
@@ -57,8 +48,8 @@ public class RaidTestScript : MonoBehaviour
     public bool CheckRaidPool(out Raid raid)
     {
         raid = null;
-        if(m_raidList.Count == 0) return false;
-        raid = m_raidList.Find(x => !x.gameObject.activeSelf);
+        if(m_RaidList.Count == 0) return false;
+        raid = m_RaidList.Find(x => !x.gameObject.activeSelf);
         return raid != null;
     }
 }
